91bc98f6c720b7aaa3f48b4f60b72da88ff4cae1
scalalang2 <cailhuiris@gmail.com>
2022-12-26 15:05:06 +0900

    feat(client): default cosmos query client implementation

---
+++ client/client.go
@@ -0,0 +1,207 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/store/rootmulti"
+	abci "github.com/tendermint/tendermint/abci/types"
+	rpcclient "github.com/tendermint/tendermint/rpc/client"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/encoding"
+	"google.golang.org/grpc/encoding/proto"
+	"google.golang.org/grpc/metadata"
+	"reflect"
+	"strconv"
+	"strings"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+)
+
+type Config struct {
+	RPCEndpoint string
+}
+
+var protoCodec = encoding.GetCodec(proto.Name)
+
+// Client is a client for the cosmos-sdk based blockchain
+// This client is only used to query the blockchain since it has no functionality to invoke a transaction
+type Client struct {
+	log *zap.SugaredLogger
+	cfg *Config
+	rpc rpcclient.Client
+}
+
+func NewClient(cfg *Config) (*Client, error) {
+	rpcClient, err := NewRPCClient(cfg.RPCEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		log: zap.L().Sugar(),
+		cfg: cfg,
+		rpc: rpcClient,
+	}, nil
+}
+
+func NewRPCClient(address string) (*rpchttp.HTTP, error) {
+	rpcClient, err := rpchttp.New(address)
+	if err != nil {
+		return nil, err
+	}
+	return rpcClient, nil
+}
+
+// Invoke implements the grpc ClientConn.Invoke method
+func (c *Client) Invoke(ctx context.Context, method string, req, reply interface{}, opts ...grpc.CallOption) (err error) {
+	if reflect.ValueOf(req).IsNil() {
+		return sdkerrors.ErrInvalidRequest.Wrap("request cannot be nil")
+	}
+
+	if _, ok := req.(*tx.BroadcastTxRequest); ok {
+		return sdkerrors.ErrInvalidRequest.Wrap("broadcasting transactions is not supported")
+	}
+
+	inMd, _ := metadata.FromOutgoingContext(ctx)
+	abciRes, outMd, err := c.RunGRPCQuery(ctx, method, req, inMd)
+	if err != nil {
+		return err
+	}
+
+	if err = protoCodec.Unmarshal(abciRes.Value, reply); err != nil {
+		return err
+	}
+
+	for _, callOpt := range opts {
+		header, ok := callOpt.(grpc.HeaderCallOption)
+		if !ok {
+			continue
+		}
+
+		*header.HeaderAddr = outMd
+	}
+
+	return nil
+}
+
+// NewStream implements the grpc ClientConn.NewStream method
+func (c *Client) NewStream(context.Context, *grpc.StreamDesc, string, ...grpc.CallOption) (grpc.ClientStream, error) {
+	return nil, fmt.Errorf("streaming rpc not supported")
+}
+
+// RunGRPCQuery runs a gRPC query from the clientCtx, given all necessary
+// arguments for the gRPC method, and returns the ABCI response. It is used
+// to factorize code between client (Invoke) and server (RegisterGRPCServer)
+// gRPC handlers.
+func (c *Client) RunGRPCQuery(ctx context.Context, method string, req interface{}, md metadata.MD) (abci.ResponseQuery, metadata.MD, error) {
+	reqBz, err := protoCodec.Marshal(req)
+	if err != nil {
+		return abci.ResponseQuery{}, nil, err
+	}
+
+	// parse height header
+	if heights := md.Get(grpctypes.GRPCBlockHeightHeader); len(heights) > 0 {
+		height, err := strconv.ParseInt(heights[0], 10, 64)
+		if err != nil {
+			return abci.ResponseQuery{}, nil, err
+		}
+		if height < 0 {
+			return abci.ResponseQuery{}, nil, sdkerrors.Wrapf(
+				sdkerrors.ErrInvalidRequest,
+				"client.Context.Invoke: height (%d) from %q must be >= 0", height, grpctypes.GRPCBlockHeightHeader)
+		}
+
+	}
+
+	height, err := GetHeightFromMetadata(md)
+	if err != nil {
+		return abci.ResponseQuery{}, nil, err
+	}
+
+	prove, err := GetProveFromMetadata(md)
+	if err != nil {
+		return abci.ResponseQuery{}, nil, err
+	}
+
+	abciReq := abci.RequestQuery{
+		Path:   method,
+		Data:   reqBz,
+		Height: height,
+		Prove:  prove,
+	}
+
+	abciRes, err := c.QueryABCI(ctx, abciReq)
+	if err != nil {
+		return abci.ResponseQuery{}, nil, err
+	}
+
+	// Create header metadata. For now the headers contain:
+	// - block height
+	// We then parse all the call options, if the call option is a
+	// HeaderCallOption, then we manually set the value of that header to the
+	// metadata.
+	md = metadata.Pairs(grpctypes.GRPCBlockHeightHeader, strconv.FormatInt(abciRes.Height, 10))
+	return abciRes, md, nil
+}
+
+func (c *Client) QueryABCI(ctx context.Context, req abci.RequestQuery) (abci.ResponseQuery, error) {
+	opts := rpcclient.ABCIQueryOptions{
+		Height: req.Height,
+		Prove:  req.Prove,
+	}
+	result, err := c.rpc.ABCIQueryWithOptions(ctx, req.Path, req.Data, opts)
+	if err != nil {
+		return abci.ResponseQuery{}, err
+	}
+
+	if !result.Response.IsOK() {
+		return abci.ResponseQuery{}, fmt.Errorf("query failed: %s", result.Response.String())
+	}
+
+	// data from trusted node or subspace query doesn't need verification
+	if !opts.Prove || !isQueryStoreWithProof(req.Path) {
+		return result.Response, nil
+	}
+
+	return result.Response, nil
+}
+
+func GetHeightFromMetadata(md metadata.MD) (int64, error) {
+	height := md.Get(grpctypes.GRPCBlockHeightHeader)
+	if len(height) == 1 {
+		return strconv.ParseInt(height[0], 10, 64)
+	}
+	return 0, nil
+}
+
+func GetProveFromMetadata(md metadata.MD) (bool, error) {
+	prove := md.Get("x-cosmos-query-prove")
+	if len(prove) == 1 {
+		return strconv.ParseBool(prove[0])
+	}
+	return false, nil
+}
+
+// isQueryStoreWithProof expects a format like /<queryType>/<storeName>/<subpath>
+// queryType must be "store" and subpath must be "key" to require a proof.
+func isQueryStoreWithProof(path string) bool {
+	if !strings.HasPrefix(path, "/") {
+		return false
+	}
+
+	paths := strings.SplitN(path[1:], "/", 3)
+
+	switch {
+	case len(paths) != 3:
+		return false
+	case paths[0] != "store":
+		return false
+	case rootmulti.RequireProof("/" + paths[2]):
+		return true
+	}
+
+	return false
+}
--- go.mod
+++ go.mod
@@ -6,18 +6,35 @@ require (
 	github.com/Masterminds/squirrel v1.5.3
 	github.com/georgysavva/scany/v2 v2.0.0
 	github.com/jackc/pgx/v5 v5.2.0
+	github.com/tendermint/tendermint v0.35.9
+	github.com/cosmos/cosmos-sdk v0.46.7
 	go.uber.org/zap v1.24.0
 )
 
 require (
+	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/puddle/v2 v2.1.2 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/oasisprotocol/curve25519-voi v0.0.0-20210609091139-0a56a4bca00b // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/rs/zerolog v1.27.0 // indirect
+	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/net v0.0.0-20220617184016-355a448f1bc9 // indirect
 	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.8 // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
