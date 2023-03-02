b678013afae8e331759afe0bea5fcc2437f2a151
scalalang2 <cailhuiris@gmail.com>
2023-03-03 06:46:15 +0900

    chore: added test

--- internal/model/chain.go
+++ internal/model/chain.go
@@ -19,5 +19,20 @@ func (c *Chain) Create(db *gorm.DB) error {
 
 // FindByID find a chain by id
 func (c *Chain) FindByID(db *gorm.DB, chainId uint) error {
-	return db.Where("id = ?", chainId).First(&c).Error
+	return db.Where(Chain{
+		Model: gorm.Model{ID: chainId},
+	}).First(&c).Error
+}
+
+// FindByChainID find a chain by chainId which is unique identifier as string for a specific chain
+func (c *Chain) FindByChainID(db *gorm.DB, chainId string) error {
+	return db.Where(Chain{
+		ChainId: chainId,
+	}).First(&c).Error
+}
+
+// FindAll returns all registered chains
+func (c *Chain) FindAll(db *gorm.DB) ([]Chain, error) {
+	var chains []Chain
+	return chains, db.Find(&chains).Error
 }
