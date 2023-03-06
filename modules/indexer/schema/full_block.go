79b025b4e513e34011b4f6fa0ac41a42ca8bfe09
scalalang2 <cailhuiris@gmail.com>
2023-03-06 19:09:53 +0900

    removed existing indexer engine

--- modules/indexer/schema/full_block.go
+++ modules/indexer/schema/full_block.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"cosmscan-go/internal/model"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -25,7 +26,7 @@ func NewFullBlock(block *coretypes.ResultBlock, abciTx []*coretypes.ResultTx, co
 
 	// fill block
 	b := &model.Block{
-		Height:             block.Block.Height,
+		Height:             uint32(block.Block.Height),
 		Hash:               block.Block.Hash().String(),
 		ProposerAddress:    block.Block.ProposerAddress.String(),
 		LastCommitHash:     block.Block.LastCommitHash.String(),
@@ -51,15 +52,23 @@ func NewFullBlock(block *coretypes.ResultBlock, abciTx []*coretypes.ResultTx, co
 			return nil, err
 		}
 
+		var rawLog []byte
+		if res.TxResponse.RawLog != "" {
+			rawLog, err = json.Marshal(res.TxResponse.RawLog)
+			if err != nil {
+				return nil, fmt.Errorf("failed to marshal raw log of tx(%s), err: %w", res.TxResponse.TxHash, err)
+			}
+		}
+
 		// fill transaction
 		fullTx.Tx = &model.Transaction{
 			Hash:      tx.Hash.String(),
-			Height:    block.Block.Height,
+			Height:    uint32(block.Block.Height),
 			Seq:       int(tx.Index),
 			Code:      int(res.TxResponse.Code),
 			CodeSpace: res.TxResponse.Codespace,
 			TxData:    res.TxResponse.Data,
-			RawLog:    res.TxResponse.RawLog,
+			RawLog:    rawLog,
 			Info:      res.TxResponse.Info,
 			Memo:      res.Tx.Body.Memo,
 			GasWanted: uint64(res.TxResponse.GasWanted),
@@ -72,7 +81,7 @@ func NewFullBlock(block *coretypes.ResultBlock, abciTx []*coretypes.ResultTx, co
 			for _, event := range txLog.Events {
 				for _, attr := range event.Attributes {
 					fullTx.Events = append(fullTx.Events, &model.Event{
-						Height:  block.Block.Height,
+						Height:  uint32(block.Block.Height),
 						Seq:     txLog.MsgIndex,
 						Type:    event.Type,
 						Key:     attr.Key,
@@ -87,9 +96,13 @@ func NewFullBlock(block *coretypes.ResultBlock, abciTx []*coretypes.ResultTx, co
 		for seq, msg := range res.Tx.Body.Messages {
 			// TODO: wanna to store the message as raw json format
 			// e.g. { "type": "MsgSend", "value": { "sender": "alice", "recipient": "bob", "amount": "1000" } }
+			rawData, err := json.Marshal(msg)
+			if err != nil {
+				return nil, fmt.Errorf("failed to marshal raw message of tx(%s), seq(%d), err: %w", res.TxResponse.TxHash, seq, err)
+			}
 			fullTx.Messages = append(fullTx.Messages, &model.Message{
-				Seq:     seq,
-				RawData: fmt.Sprintf("{ \"type\": \"%s\"}", msg.TypeUrl),
+				Seq:     uint(seq),
+				RawData: rawData,
 			})
 		}
 
